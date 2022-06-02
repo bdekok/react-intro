7435d5a2db1584a1f2319edbbd1f95bf0f212fcb
Brent de Kok <brent.dekok@coolblue.nl>
2022-06-02 21:38:36 +0200

    Cats Part 1: Made the button work

--- src/App.js
+++ src/App.js
@@ -2,16 +2,28 @@ import cats from "./cats.json";
 import CatBackground from "./components/CatBackground/CatBackground";
 import CatInfo from "./components/CatInfo/CatInfo";
 import CatButton from "./components/CatButton/CatButton";
+import { useState } from "react";
 
 function CatApp() {
-  const currentCat = cats[0];
+  const [currentCatIndex, setCatIndex] = useState(0);
+  const currentCat = cats[currentCatIndex];
   const currentPicture = currentCat.image.url;
+
+  function getNextCat() {
+    const isLastCat = currentCatIndex === cats.length - 1;
+
+    if (isLastCat) {
+      setCatIndex(0);
+    } else {
+      setCatIndex(currentCatIndex + 1);
+    }
+  }
 
   return (
     <>
       <CatBackground imageUrl={currentPicture} />
       <CatInfo cat={currentCat}>
-        <CatButton>Next Cat</CatButton>
+        <CatButton onClick={getNextCat}>Next Cat</CatButton>
       </CatInfo>
     </>
   );
--- src/components/CatButton/CatButton.jsx
+++ src/components/CatButton/CatButton.jsx
@@ -1,5 +1,5 @@
 import styles from "./CatButton.module.css";
 
-export default function CatButton({children}) {
-  return <button className={styles.button}>{children}</button>;
+export default function CatButton({children, onClick}) {
+  return <button className={styles.button} onClick={onClick}>{children}</button>;
 }
